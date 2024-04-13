facfda6618f5abe5123393eed9dee56b68daa036
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-04-13 02:45:28 +0000

    quede en el de eiminar

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = [
+    {"label": "My first Task", "done": False}
+]
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    body = request.get_json()
+    request_body = request.json
+    todos.append(body)
+    print('Incoming request with the following body', request_body)
+    return jsonify(todos), 200
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todos(position):
+    print('This is the position to delete', position)
+    return 'something'
+
+
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port=3245, debug = True)
